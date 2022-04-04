67c55bea44506ec0c9fa91d26c0153648c346a1f
nurulamin092 <mnurulamin092@gmial.com>
2022-04-04 23:27:08 +0600

    change react 17 update 18 version

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOMClient from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+const root = ReactDOMClient.createRoot(document.getElementById('root'))
 
-ReactDOM.render(
+root.render(
   <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </BrowserRouter>,
-  document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
